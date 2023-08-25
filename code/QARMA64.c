756a0f6c4d6b246954d7820ec96b02b3c7b8bb90
Kamel Fakih <kamel.fakih@studenti.polito.it>
2023-08-25 11:58:48 +0000

    compile as a static library

--- code/QARMA64.c
+++ code/QARMA64.c
@@ -1,53 +1,5 @@
 // Copyright (c) 2019-2022 Phantom1003
-
-#include <stdio.h>
-
-#define MAX_LENGTH 64
-#define subcells sbox[sbox_use]
-#define subcells_inv sbox_inv[sbox_use]
-
-typedef unsigned long long int const_t;
-typedef unsigned long long int tweak_t;
-typedef unsigned long long int text_t;
-typedef unsigned long long int key_t;
-typedef unsigned char          cell_t;
-
-//int sbox_use = 0;
-//text_t check_box[3] = { 0x3ee99a6c82af0c38, 0x9f5c41ec525603c9, 0xbcaf6c89de930765 };
-
-//int sbox_use = 1;
-//text_t check_box[3] = { 0x544b0ab95bda7c3a, 0xa512dd1e4e3ec582, 0xedf67ff370a483f2 };
-
-int sbox_use = 2;
-text_t check_box[3] = { 0xc003b93999b33765, 0x270a787275c48d10, 0x5c06a7501b63b2fd };
-
-
-int m = MAX_LENGTH / 16;
-
-const_t alpha = 0xC0AC29B7C97C50DD;
-const_t c[8] = { 0x0000000000000000, 0x13198A2E03707344, 0xA4093822299F31D0, 0x082EFA98EC4E6C89,
-				 0x452821E638D01377, 0xBE5466CF34E90C6C, 0x3F84D5B5B5470917, 0x9216D5D98979FB1B };
-
-int sbox[3][16] = { { 0, 14,  2, 10,  9, 15,  8, 11,  6,  4,  3,  7, 13, 12,  1,  5},
-					{10, 13, 14,  6, 15,  7,  3,  5,  9,  8,  0, 12, 11,  1,  2,  4},
-					{11,  6,  8, 15, 12,  0,  9, 14,  3,  7,  4,  5, 13,  2,  1, 10} };
-
-int sbox_inv[3][16] = { { 0, 14,  2, 10,  9, 15,  8, 11,  6,  4,  3,  7, 13, 12,  1,  5},
-						{10, 13, 14,  6, 15,  7,  3,  5,  9,  8,  0, 12, 11,  1,  2,  4},
-						{ 5, 14, 13,  8, 10, 11,  1,  9,  2,  6, 15,  0,  4, 12,  7,  3} };
-
-int t[16] = { 0, 11,  6, 13, 10,  1, 12,  7,  5, 14,  3,  8, 15,  4,  9,  2 };
-int t_inv[16] = { 0,  5, 15, 10, 13,  8,  2,  7, 11, 14,  4,  1,  6,  3,  9, 12 };
-int h[16] = { 6,  5, 14, 15,  0,  1,  2,  3,  7, 12, 13,  4,  8,  9, 10, 11 };
-int h_inv[16] = { 4,  5,  6,  7, 11,  1,  0,  8, 12, 13, 14, 15,  9, 10,  2,  3 };
-
-#define Q M
-#define M_inv M
-cell_t M[16] = { 0, 1, 2, 1,
-				 1, 0, 1, 2,
-				 2, 1, 0, 1,
-				 1, 2, 1, 0 };
-
+#include "QARMA64.h"
 
 void text2cell(cell_t* cell, text_t is) {
 	// for 64 bits
@@ -105,12 +57,12 @@ text_t pseudo_reflect(text_t is, key_t tk) {
 }
 
 text_t forward(text_t is, key_t tk, int r) {
-	//printf("key = 0x%016llx\n", tk);
+	
 
 	is ^= tk;
 	cell_t cell[16];
 	text2cell(cell, is);
-	//printf("0x%016llx\n", is);
+	
 
 
 	if (r != 0) {
@@ -140,7 +92,7 @@ text_t forward(text_t is, key_t tk, int r) {
 		cell[i] = subcells[cell[i]];
 	}
 	is = cell2text(cell);
-	//printf("0x%016llx\n", is);
+	
 
 	return is;
 }
@@ -148,13 +100,13 @@ text_t forward(text_t is, key_t tk, int r) {
 text_t backward(text_t is, key_t tk, int r) {
 	cell_t cell[16];
 	text2cell(cell, is);
-	//printf("0x%016llx\n", is);
+	
 
 	// SubCells
 	for (int i = 0; i < 16; i++) {
 		cell[i] = subcells_inv[cell[i]];
 	}
-	//printf("key = 0x%016llx\n", tk);
+	
 
 	if (r != 0) {
 		cell_t mixc[16];
@@ -180,7 +132,7 @@ text_t backward(text_t is, key_t tk, int r) {
 
 	is = cell2text(cell);
 	is ^= tk;
-	//printf("0x%016llx\n", is);
+	
 	return is;
 }
 
@@ -251,24 +203,24 @@ text_t qarma64_enc(text_t plaintext, tweak_t tweak, key_t w0, key_t k0, int roun
 	text_t is = plaintext ^ w0;
 
 	for (int i = 0; i < rounds; i++) {
-		//printf("%d:\n", i);
+		
 		is = forward(is, k0 ^ tweak ^ c[i], i);
 		tweak = forward_update_key(tweak);
 	}
 
-	//printf("0x%016llx\n", is);
+	
 	is = forward(is, w1 ^ tweak, 1);
 	is = pseudo_reflect(is, k1);
 	is = backward(is, w0 ^ tweak, 1);
 
 	for (int i = rounds - 1; i >= 0; i--) {
-		//printf("%d:\n", i);
+		
 		tweak = backward_update_key(tweak);
 		is = backward(is, k0 ^ tweak ^ c[i] ^ alpha, i);
 	}
 
 	is ^= w1;
-	//printf("0x%016llx\n", is);
+	
 	return is;
 }
 
@@ -293,64 +245,29 @@ text_t qarma64_dec(text_t plaintext, tweak_t tweak, key_t w0, key_t k0, int roun
 			k1_cell[4 * x + y] = temp;
 		}
 	}
+
 	key_t k1 = cell2text(k1_cell);
 
 	k0 ^= alpha;
-
 	text_t is = plaintext ^ w0;
 
 	for (int i = 0; i < rounds; i++) {
-		//printf("%d:\n", i);
+		
 		is = forward(is, k0 ^ tweak ^ c[i], i);
 		tweak = forward_update_key(tweak);
 	}
 
-	//printf("0x%016llx\n", is);
+	
 	is = forward(is, w1 ^ tweak, 1);
 	is = pseudo_reflect(is, k1);
 	is = backward(is, w0 ^ tweak, 1);
 
 	for (int i = rounds - 1; i >= 0; i--) {
-		//printf("%d:\n", i);
+		
 		tweak = backward_update_key(tweak);
 		is = backward(is, k0 ^ tweak ^ c[i] ^ alpha, i);
 	}
 
-	is ^= w1;
-	//printf("0x%016llx\n", is);
+	is ^= w1;	
 	return is;
 }
-
-
-
-
-int main() {
-	text_t plaintext = 0xfb623599da6e8127;
-	key_t w0 = 0x84be85ce9804e94b;
-	key_t k0 = 0xec2802d4e0a488e9;
-	tweak_t tweak = 0x477d469dec0b8762;
-	text_t ciphertext;
-
-	printf("QARMA64  Use Sbox %d\nPlaintext = 0x%016llx\nKey = 0x%016llx || 0x%016llx\nTweak = 0x%016llx\n\n", sbox_use, plaintext, w0, k0, tweak);
-
-
-	ciphertext = qarma64_enc(plaintext, tweak, w0, k0, 5);
-	printf("Round 5: \nCiphertext = 0x%016llx  %s\n", ciphertext, (ciphertext == check_box[0]) ? "√" : "×");
-	ciphertext = qarma64_dec(ciphertext, tweak, w0, k0, 5);
-	printf("Plaintext  = 0x%016llx  %s\n", ciphertext, (ciphertext == plaintext) ? "√" : "×");
-
-
-	ciphertext = qarma64_enc(plaintext, tweak, w0, k0, 6);
-	printf("Round 6: \nCiphertext = 0x%016llx  %s\n", ciphertext, (ciphertext == check_box[1]) ? "√" : "×");
-	ciphertext = qarma64_dec(ciphertext, tweak, w0, k0, 6);
-	printf("Plaintext  = 0x%016llx  %s\n", ciphertext, (ciphertext == plaintext) ? "√" : "×");
-
-
-	ciphertext = qarma64_enc(plaintext, tweak, w0, k0, 7);
-	printf("Round 7: \nCiphertext = 0x%016llx  %s\n", ciphertext, (ciphertext == check_box[2]) ? "√" : "×");
-	ciphertext = qarma64_dec(ciphertext, tweak, w0, k0, 7);
-	printf("Plaintext  = 0x%016llx  %s\n", ciphertext, (ciphertext == plaintext) ? "√" : "×");
-
-}
-
-
---
+++ makefile
@@ -0,0 +1,40 @@
+CC = $(LLVM_PATH)/clang --target=aarch64-linux-gnu -fuse-ld=lld --sysroot=$(AARCH64_SYSROOT)
+AR = $(LLVM_PATH)/llvm-ar
+
+CFLAGS     = 
+INC_FLAGS  = -I./include
+
+SRC_DIR   = ./code
+BUILD_DIR = ./build
+OBJS_DIR  = $(BUILD_DIR)/objs
+LIB_DIR   = $(BUILD_DIR)/lib
+
+TARGET    = libQarma64.a 
+
+SRC_FILES     = $(wildcard $(SRC_DIR)/*.c)
+OBJ_FILES     = $(patsubst $(SRC_DIR)/%.c, $(OBJS_DIR)/%.o, $(SRC_FILES))
+DEPENDENCIES  = $(OBJ_FILES:.o=.d)
+
+# Build rules
+$(LIB_DIR)/$(TARGET): $(OBJ_FILES)
+	@mkdir -p $(LIB_DIR)
+	$(AR) rcs $@ $(OBJ_FILES)
+
+
+$(OBJS_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) $(INC_FLAGS) -MMD -MP -c $< -o $@
+
+# Phony targets
+.PHONY: info clean
+
+info: 
+	$(info $(SRC_FILES))
+	$(info $(OBJ_FILES))
+	$(info $(DEPENDENCIES))
+
+# Clean
+clean:
+	$(RM) -r $(BUILD_DIR)
+
+-include $(DEPENDENCIES)
